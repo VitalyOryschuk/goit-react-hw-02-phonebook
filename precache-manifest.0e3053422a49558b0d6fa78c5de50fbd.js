4d13f72b18fedbfe5c634abb19e95f527ab5fe59
Vitaly Oryschuk <Leilyua@ukr.net>
2020-01-12 10:14:54 +0200

    Updates

--- precache-manifest.0e3053422a49558b0d6fa78c5de50fbd.js
+++ precache-manifest.0e3053422a49558b0d6fa78c5de50fbd.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "4c1254d94a66b552a4df4a5b40b924ba",
+    "revision": "0a49e0463a043fbf2f91b5f87850ff60",
     "url": "/goit-react-hw-02-phonebook/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/goit-react-hw-02-phonebook/static/css/2.764ccc25.chunk.css"
   },
   {
-    "revision": "3322c4d803b9e7dff341",
+    "revision": "ee6ad56954fdf503cb0b",
     "url": "/goit-react-hw-02-phonebook/static/css/main.dfcaf89c.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/goit-react-hw-02-phonebook/static/js/2.d4ba3a1e.chunk.js.LICENSE"
   },
   {
-    "revision": "3322c4d803b9e7dff341",
-    "url": "/goit-react-hw-02-phonebook/static/js/main.86d51dc3.chunk.js"
+    "revision": "ee6ad56954fdf503cb0b",
+    "url": "/goit-react-hw-02-phonebook/static/js/main.0ce1e7e6.chunk.js"
   },
   {
     "revision": "86e8f61da2e8edff8d43",
